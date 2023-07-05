e9804aebdd90cc9105edbcd55ba0093618c95c4a
ovtoubia <ovtoubia.20@est.ucab.edu.ve>
2023-07-05 19:03:34 -0400

    mis install no se ignorenlo

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\LAGD\Documents\Clases UCAB\7mo Semestre\Redes II\Proyecto_subastas\frontend\venv
+set VIRTUAL_ENV=C:\Users\User\OneDrive\Documents\GitHub\Proyecto_Subastas\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
